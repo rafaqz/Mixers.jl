3030f654dc5cebbfa4c598e3ab6523729e7cfacd
Tamas Gal <tgal@km3net.de>
2020-04-27 00:24:55 +0200

    Add @stack which stacks multiple structs into one

--- test/runtests.jl
+++ test/runtests.jl
@@ -136,3 +136,24 @@ function sayhi()
    @hello
 end
 @test sayhi() == "Hello world"                                                                      
+
+
+# @stack
+struct A
+    n::Int32
+    m::Int64
+end
+struct B
+    o::Float16
+    p
+    q::Bool
+end
+struct C
+    r
+    s
+    t
+    u
+end
+@stack D A B C
+@test fieldnames(D) == (:n, :m, :o, :p, :q, :r, :s, :t, :u)
+@test fieldtypes(D) == (Int32, Int64, Float16, Any, Bool, Any, Any, Any, Any)
