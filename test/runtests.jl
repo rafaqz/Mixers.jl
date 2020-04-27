8fdc89eba58d2d7b51d2a0872325642eb4da5a60
Tamas Gal <tgal@km3net.de>
2020-04-27 09:15:48 +0200

    Fix @stack tests for Julia 1.0

--- test/runtests.jl
+++ test/runtests.jl
@@ -155,5 +155,7 @@ struct C
     u
 end
 @stack D A B C
-@test fieldnames(D) == (:n, :m, :o, :p, :q, :r, :s, :t, :u)
-@test fieldtypes(D) == (Int32, Int64, Float16, Any, Bool, Any, Any, Any, Any)
+expected_fieldnames = (:n, :m, :o, :p, :q, :r, :s, :t, :u)
+expected_fieldtypes = [Int32, Int64, Float16, Any, Bool, Any, Any, Any, Any]
+@test fieldnames(D) == expected_fieldnames
+@test [fieldtype(D, f) for f in fieldnames(D)] == expected_fieldtypes
